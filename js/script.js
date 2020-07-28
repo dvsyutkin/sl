c6ab0228a81802c28f8afad16834b65ae416d4e2
denis <s.d-v@mail.ru>
2020-07-28 15:18:32 +0300

    4

---
+++ js/script.js
@@ -0,0 +1,29 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+    // слайдер компаний
+    const sliderTops = $('.tops__wrap .owl-carousel');
+    sliderTops.owlCarousel($.extend({}, {
+        nav: false,
+        dots: true,
+        margin: 50,
+        loop: true,
+        center:true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            720: {
+                items: 3
+            },
+            960: {
+                items: 3.3
+            },
+            1440: {
+                items: 4.5
+            }
+        },
+
+    }));
+
+
+});
