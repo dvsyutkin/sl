a3377149be5ff87b26389c2f6576d4db92ba0e4a
denis <s.d-v@mail.ru>
2020-07-29 16:56:11 +0300

    6

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 window.addEventListener('DOMContentLoaded', function () {
 
-    // слайдер компаний
-    const sliderTops = $('.tops__wrap .owl-carousel');
+    // слайдер ТОП - предложения
+    const   sliderTops = $('.tops__wrap .owl-carousel'),
+            topsPrev = document.querySelector('.tops .section__arrow_prev'),
+            topsNext = document.querySelector('.tops .section__arrow_next');
     sliderTops.owlCarousel($.extend({}, {
         dots: true,
         margin: 50,
@@ -23,6 +25,78 @@ window.addEventListener('DOMContentLoaded', function () {
         }
 
     }));
+    topsNext.addEventListener('click', function () {
+        sliderTops.trigger('next.owl.carousel');
+    });
+    topsPrev.addEventListener('click', function () {
+        sliderTops.trigger('prev.owl.carousel');
+    });
+
+    // слайдер Акции
+    const   sliderStock = $('.stock__wrap .owl-carousel'),
+            stockPrev = document.querySelector('.stock .section__arrow_prev'),
+            stockNext = document.querySelector('.stock .section__arrow_next');
+    sliderStock.owlCarousel($.extend({}, {
+        dots: true,
+        margin: 50,
+        loop: true,
+        center:true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            720: {
+                items: 3
+            },
+            960: {
+                items: 3.3
+            },
+            1440: {
+                items: 4.5
+            }
+        }
+
+    }));
+    stockNext.addEventListener('click', function () {
+        sliderStock.trigger('next.owl.carousel');
+    });
+    stockPrev.addEventListener('click', function () {
+        sliderStock.trigger('prev.owl.carousel');
+    });
+
+    // слайдер Тоавры под заказ
+    const   sliderOrder = $('.order__wrap .owl-carousel'),
+            orderPrev = document.querySelector('.order .section__arrow_prev'),
+            orderNext = document.querySelector('.order .section__arrow_next');
+    sliderOrder.owlCarousel($.extend({}, {
+        dots: true,
+        margin: 50,
+        loop: true,
+        center:true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            720: {
+                items: 3
+            },
+            960: {
+                items: 3.3
+            },
+            1440: {
+                items: 4.5
+            }
+        }
+
+    }));
+    orderNext.addEventListener('click', function () {
+        sliderOrder.trigger('next.owl.carousel');
+    });
+    orderPrev.addEventListener('click', function () {
+        sliderOrder.trigger('prev.owl.carousel');
+    });
+
+    
 
 
 
